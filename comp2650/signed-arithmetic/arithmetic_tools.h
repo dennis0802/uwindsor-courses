e806918f29658cafec29eee226f76bdba5bde6d1
dennis0802 <79319610+dennis0802@users.noreply.github.com>
2021-02-19 10:09:20 -0500

    Add files via upload

---
+++ comp2650/signed-arithmetic/arithmetic_tools.h
@@ -0,0 +1,11 @@
+//============================================================================
+// Name        : convert_tools.h
+// Author      : Dennis Dao
+// Version     : 1.0
+// Lab Section : 55
+// Description : Header file for arithmetic functions
+//============================================================================
+
+void func_signed_mag_addition(int a[], int b[], int result[]);
+void func_signed_mag_subtraction(int a[], int b[], int result[]);
+
