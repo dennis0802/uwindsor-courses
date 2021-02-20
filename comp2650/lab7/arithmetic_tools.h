bbd245ef753c955b5ca09a0c5cd68a7bceb4951d
dennis0802 <79319610+dennis0802@users.noreply.github.com>
2021-02-19 19:34:30 -0500

    Add files via upload

---
+++ comp2650/lab7/arithmetic_tools.h
@@ -0,0 +1,10 @@
+//============================================================================
+// Name        : convert_tools.h
+// Author      : Dennis Dao
+// Version     : 1.0
+// Lab Section : 55
+// Description : Header file for arithmetic functions
+//============================================================================
+
+void func_increment(int a[], int result[]);
+
