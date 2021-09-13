f4bb152feb48a0fc5ecc3fc089472335d68316c5
Dennis <79319610+dennis0802@users.noreply.github.com>
2021-09-13 09:33:50 -0400

    Add files via upload

---
+++ comp2560/Assignment2/test.c
@@ -0,0 +1,13 @@
+// Here is a c program
+// This is a single-line comment
+
+/***** This was made by a Uwindsor Student
+An example made on Janaury 31
+This is a block comment to print many lines
+*******************************************/
+
+#include <stdio.h>
+int main(void){
+	puts("Hello world"); // print hello message
+	return 0;
+}
